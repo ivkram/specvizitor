cd55d5e1889c56ab30f2861be4107640f0d7b2bf
ivkram <im.kramarenko@gmail.com>
2023-01-28 00:26:17 +0100

    Deleted unused catalogues

--- fresco-gui/main.py
+++ fresco-gui/main.py
@@ -50,38 +50,6 @@ def pix_scale(header):
     return scale_asec
 
 
-def write_output(input_cat, comments, new_catalog):
-    # write identifications in fits file
-
-    # make sure that the comments don't get truncated
-    max_len = 0
-    for comment in comments:
-        if len(comment) > max_len:
-            max_len = len(comment)
-    max_len += 1
-
-    columns = []
-    for column in input_cat.colnames:
-        dtype = type(input_cat[column][0])
-
-        if dtype is np.float64:
-            format="D"
-        elif dtype is np.int32:
-            format="J"
-        elif dtype is str:
-            format="20A"
-        else:
-            format="10A"
-        c = fits.Column(name=column, format=format, array=input_cat[column])
-        columns.append(c)
-
-    c = fits.Column(name='comments', format=str(max_len)+"A", array=comments)
-    columns.append(c)
-
-    table_hdu = fits.BinTableHDU.from_columns(columns)
-    table_hdu.writeto(new_catalog,overwrite=True)
-
-
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super().__init__()
