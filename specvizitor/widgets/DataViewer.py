0d2a731975b000c2fa7fce8cd181e198f6d07b48
ivkram <im.kramarenko@gmail.com>
2023-03-01 18:49:25 +0100

    Reset dock position after adding a new dock

--- specvizitor/widgets/DataViewer.py
+++ specvizitor/widgets/DataViewer.py
@@ -17,6 +17,7 @@ class DataViewer(AbstractWidget):
         super().__init__(cfg=cfg, parent=parent)
 
         self.rd = rd
+        self.cfg = cfg
 
         if plugins is not None:
             self._plugins = [importlib.import_module("specvizitor.plugins." + plugin_name).Plugin() for plugin_name in plugins]
@@ -27,38 +28,52 @@ class DataViewer(AbstractWidget):
         self.layout.setContentsMargins(0, 0, 0, 0)
 
         self.dock_area = DockArea()
-        self.docks: dict[str, Dock] = {}
-        self.dock_widgets: dict[str, ViewerElement] = {}
-
-        # create widgets for images (e.g. image cutouts, 2D spectra)
-        for name, image_cfg in cfg.images.items():
-            self.dock_widgets[name] = Image2D(rd=rd, cfg=image_cfg, name=name, parent=self)
-
-        # create widgets for 1D spectra
-        for name, spec_cfg in cfg.spectra.items():
-            self.dock_widgets[name] = Spec1D(rd=rd, cfg=spec_cfg, name=name, parent=self)
-
-        for name, widget in self.dock_widgets.items():
-            dock = Dock(name)
-            dock.addWidget(widget)
-            self.docks[name] = dock
+        self.dock_widgets = self.create_widgets()
+        self.docks = self.create_docks()
 
     def init_ui(self):
         self.layout.addWidget(self.dock_area, 1, 1, 1, 1)
+        self.add_docks()
 
+        try:
+            # set `extra` to None to catch an exception (KeyError) when adding extra docks not mentioned in `state`
+            self.dock_area.restoreState(self.rd.cache.dock_state, missing='ignore', extra=None)
+        except (KeyError, ValueError, TypeError):
+            for d in self.docks.values():
+                d.close()
+            self.docks = self.create_docks()
+            self.add_docks()
+
+        for w in self.dock_widgets.values():
+            w.init_ui()
+
+    def create_widgets(self) -> dict[str, ViewerElement]:
+        widgets = {}
+
+        # create widgets for images (e.g. image cutouts, 2D spectra)
+        for name, image_cfg in self.cfg.images.items():
+            widgets[name] = Image2D(rd=self.rd, cfg=image_cfg, name=name, parent=self)
+
+        # create widgets for 1D spectra
+        for name, spec_cfg in self.cfg.spectra.items():
+            widgets[name] = Spec1D(rd=self.rd, cfg=spec_cfg, name=name, parent=self)
+
+        return widgets
+
+    def create_docks(self) -> dict[str, Dock]:
+        docks = {}
+        for name, widget in self.dock_widgets.items():
+            dock = Dock(name)
+            dock.addWidget(widget)
+            docks[name] = dock
+        return docks
+
+    def add_docks(self):
         for name, d in self.docks.items():
             position = self.dock_widgets[name].cfg.position
             self.dock_area.addDock(dock=d,
                                    position=position if position is not None else 'bottom',
                                    relativeTo=self.dock_widgets[name].cfg.relative_to)
-
-        try:
-            self.dock_area.restoreState(self.rd.cache.dock_state, missing='ignore')
-        except (ValueError, TypeError):
-            self.save_dock_state()
-
-        for w in self.dock_widgets.values():
-            w.init_ui()
 
     def save_dock_state(self):
         self.rd.cache.dock_state = self.dock_area.saveState()
