9fc84c7b073d5185b36bda272817c9bdedef6f9a
ivkram <im.kramarenko@gmail.com>
2024-10-26 19:56:59 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'specvizitor'
 copyright = '2024, Ivan Kramarenko, Josie Kerutt'
 author = 'Ivan Kramarenko, Josie Kerutt'
-release = '0.4.2'
+release = '0.4.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
